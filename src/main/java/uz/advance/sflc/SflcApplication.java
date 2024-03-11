a9b276176f000a7fe4bc64a311382baf8ad4145a
AkbarshoxOtaboev <otaboevakbar96@gmail.com>
2024-03-12 00:05:32 +0500

    Create empty project

---
+++ src/main/java/uz/advance/sflc/SflcApplication.java
@@ -0,0 +1,13 @@
+package uz.advance.sflc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SflcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SflcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/advance/sflc/SflcApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.advance.sflc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SflcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
